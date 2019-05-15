aa275d0e1b5da4c842bd31d3d288b9d3923a0680
Hsiao-Tung Dong <s103060011@m103.nthu.edu.tw>
2019-05-15 12:10:29 +0800

    Create a simple client interface application
    - Create layout template with navbar
    - Create home page, login page, and account page
    - Allow users to login using the login page
    	- Use a service object call to authenticate
    	- Use cookie-based sessions to store the logged-in info
    - Add flash messages for errors and notices

---
+++ app/controllers/auth.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'roda'
+require_relative './app'
+
+module CoEditPDF
+  # Base class for CoEditPDF Web Application
+  class App < Roda
+    route('auth') do |routing|
+      @login_route = '/auth/login'
+      routing.is 'login' do
+        # GET /auth/login
+        routing.get do
+          view :login
+        end
+
+        # POST /auth/login
+        routing.post do
+          account = AuthenticateAccount.new(App.config).call(
+            name: routing.params['username'],
+            password: routing.params['password']
+          )
+
+          session[:current_account] = account
+          flash[:notice] = "Welcome back #{account['name']}!"
+          routing.redirect '/'
+        rescue AuthenticateAccount::UnauthorizedError
+          flash[:error] = 'Username and password did not match our records'
+          routing.redirect @login_route
+        rescue StandardError
+          flash[:error] = 'Internal error, please try again later'
+          routing.redirect @login_route
+        end
+      end
+
+      routing.on 'logout' do
+        # GET /auth/logout
+        routing.get do
+          session[:current_account] = nil
+          routing.redirect @login_route
+        end
+      end
+    end
+  end
+end
