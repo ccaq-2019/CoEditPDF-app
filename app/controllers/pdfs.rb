1e33027f0bd2fb886bd3eab7a4601e920299bfee
u10000129 <u10000129@gmail.com>
2019-06-17 19:29:20 +0800

    set add/collaborators button

--- app/controllers/pdfs.rb
+++ app/controllers/pdfs.rb
@@ -8,6 +8,41 @@ module CoEditPDF
     route('pdfs') do |routing|
       routing.on do
         routing.redirect '/auth/login' unless @current_account.logged_in?
+        @pdfs_route = '/pdfs'
+
+        routing.on(String) do |pdf_id|
+          @pdf_route = "#{@pdfs_route}/#{pdf_id}"
+
+          # POST /pdfs/[pdf_id]/collaborators
+          routing.post('collaborators') do
+            action = routing.params['action']
+            collaborator_info = Form::CollaboratorEmail.call(routing.params)
+            if collaborator_info.failure?
+              flash[:error] = Form.validation_errors(collaborator_info)
+              routing.halt
+            end
+
+            task_list = {
+              'add' => { service: AddCollaborator,
+                         message: 'Added new collaborator to pdf' },
+              'remove' => { service: RemoveCollaborator,
+                            message: 'Removed collaborator from pdf' }
+            }
+
+            task = task_list[action]
+            task[:service].new(App.config).call(
+              current_account: @current_account,
+              collaborator: collaborator_info,
+              pdf_id: pdf_id
+            )
+            flash[:notice] = task[:message]
+
+          rescue StandardError
+            flash[:error] = 'Could not find collaborator'
+          ensure
+            routing.redirect @pdfs_route
+          end
+        end
 
         # GET /pdfs/
         routing.get do
